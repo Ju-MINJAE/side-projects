fc6ecc13993db3cd5238d1d3bfa1655af337531a
Ju-MINJAE <juminjae98@gmail.com>
2024-09-14 15:48:11 +0900

    npm install -D tailwindcss postcss autoprefixer

---
+++ todo-supabase/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      height: {
+        124: '30rem',
+        160: '40rem',
+      },
+    },
+  },
+  plugins: [],
+};
